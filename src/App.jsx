71a17c5a7e53026f0cb5ceb4f00b8a5c8531d481
vynas <hamdabousnina@gmail.com>
2025-05-29 18:21:56 -0400

    dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Gallery from "./Gallery";
+import SearchForm from "./SearchForm";
+import ThemeToggle from "./ThemeToggle";
+
 function App() {
-  return <h1>Unsplash Images</h1>;
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
 }
 
 export default App;
