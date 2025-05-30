4df51a2842cb1bf79ba68d3a6fabbe72fbf4ded9
vynas <hamdabousnina@gmail.com>
2025-05-30 07:00:52 -0400

    render images

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,7 +1,56 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useGlobalContext } from "./context";
+
 const url =
-  "https://api.unsplash.com/search/photos?client_id=cQ7yZwxDQa6oRqUPMaq8ZbfXFm3tK6DJgnOVZTG5Unc&query=office";
+  "https://api.unsplash.com/search/photos?client_id=XFm3tK6DJgnOVZTG5Unc";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const { searchTerm } = useGlobalContext();
+  const response = useQuery({
+    queryKey: ["images", searchTerm],
+    queryFn: async () => {
+      const res = await axios.get(`${url}&query=${searchTerm}`);
+
+      return res.data;
+    },
+  });
+  if (response.isPending) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>Error occurred, try again later</h4>
+      </section>
+    );
+  }
+  const results = response.data.results;
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    );
+  }
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            alt={item.alt_description}
+            key={item.id}
+            className="img"
+          />
+        );
+      })}
+    </section>
+  );
 };
 export default Gallery;
