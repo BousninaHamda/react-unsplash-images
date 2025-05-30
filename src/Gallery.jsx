f2104ce5f4be87bc672c7ee1e8418d3de4d7013b
vynas <hamdabousnina@gmail.com>
2025-05-30 06:26:58 -0400

    setup query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,3 +1,6 @@
+const url =
+  "https://api.unsplash.com/search/photos?client_id=cQ7yZwxDQa6oRqUPMaq8ZbfXFm3tK6DJgnOVZTG5Unc&query=office";
+
 const Gallery = () => {
   return <div>Gallery</div>;
 };
